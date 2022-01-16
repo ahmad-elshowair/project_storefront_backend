251b7f3db6ba0de4c460f75c90c9ba240a63f4e5
Ahmad Elshowair <it.is.work@outlook.com>
2022-01-16 15:11:24 +0700

    modify user testing file

--- src/tests/models/Users.Spec.ts
+++ src/tests/models/Users.Spec.ts
@@ -1,71 +1,77 @@
-// import { User, UserModel } from "../../Models/User";
+import { User, UserModel } from '../../Models/User';
 
+const model = new UserModel();
+/* ============================== START UNIT TEST ============================== */
+describe('Test User model', () => {
+  /* ==============================  BEGIN ARE CRUD FUNCTIONS DECLARED?  ============================== */
 
-// const model = new UserModel();
-// /* ============================== START UNIT TEST ============================== */
-// describe('Test User model', () => {
-// 	/* ==============================  BEGIN ARE CRUD FUNCTIONS DECLARED?  ============================== */
+  describe('check crud functions', () => {
+    it('index method should be declared', () => {
+      expect(model.index).toBeDefined();
+    });
 
-// 	describe('check crud functions', () => {
-// 		it('index method should be declared', () => {
-// 			expect(model.index).toBeDefined();
-// 		});
+    it('show method should be declared', () => {
+      expect(model.show).toBeDefined();
+    });
 
-// 		it('show method should be declared', () => {
-// 			expect(model.show).toBeDefined();
-// 		});
+    it('create method should be declared', () => {
+      expect(model.create).toBeDefined();
+    });
 
-// 		it('create method should be declared', () => {
-// 			expect(model.create).toBeDefined();
-// 		});
+    it('update method should be declared', () => {
+      expect(model.update).toBeDefined();
+    });
 
-// 		it('update method should be declared', () => {
-// 			expect(model.update).toBeDefined();
-// 		});
+    it('delete method should be declared', () => {
+      expect(model.delete).toBeDefined();
+    });
 
-// 		it('delete method should be declared', () => {
-// 			expect(model.delete).toBeDefined();
-// 		});
+    it('login method should be declared', () => {
+      expect(model.login).toBeDefined();
+    });
+  });
+  /* ==============================  END ARE CRUD FUNCTIONS DECLARED?  ============================== */
 
-// 		it('login method should be declared', () => {
-// 			expect(model.login).toBeDefined();
-// 		});
-// 	});
-// 	/* ==============================  END ARE CRUD FUNCTIONS DECLARED?  ============================== */
+  /* ==============================  BEGIN CRUD FUNCTIONALITY  ============================== */
+  describe('Test CRUD functionality', () => {
+    const userData: User = {
+      id: 1,
+      first_name: 'Thuy',
+      last_name: 'Pham',
+      email: 'thuy@email.com',
+      password_digest: 'password',
+    };
+    let user: User;
 
-// 	/* ==============================  BEGIN CRUD FUNCTIONALITY  ============================== */
-// 	describe('Test CRUD functionality', () => {
-// 		const userData: User = {
-// 			first_name: 'Thuy',
-// 			last_name: 'Pham',
-// 			email: 'thuy@email.com',
-// 			password_digest: 'password',
-// 		};
-// 		let user: User;
-	
-// 		/* test create method functionality*/
-// 		it('should create a new user', async () => {
-// 			user = await model.create(userData);
-// 			expect({
-// 				first_name: user.first_name,
-// 				last_name: user.last_name,
-// 				email: user.email,
-// 				password_digest: user.password_digest
-// 			}).toEqual({
-// 				first_name: userData.first_name,
-// 				last_name: userData.last_name,
-// 				email: userData.email,
-// 				password_digest: userData.password_digest,
-// 			});
-// 		});
+    /* test create method functionality*/
+    it('should create a new user', async () => {
+      user = await model.create(userData);
+      expect({
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+        password_digest: user.password_digest,
+      }).toEqual({
+        first_name: userData.first_name,
+        last_name: userData.last_name,
+        email: userData.email,
+        password_digest: userData.password_digest,
+      });
+    });
 
-// 		/* test user index method functionality*/
-// 		it('index method should return users', async () => {
-// 			const users = await model.index();
-// 			expect(users).toEqual([user]);
-// 		});
-// 	});
-// 	/* ==============================  END CRUD FUNCTIONALITY  ============================== */
-// });
-// /* ============================== END UNIT TEST ============================== */
-    
+    /* test user index method functionality*/
+    it('index method should return users', async () => {
+      const users = await model.index();
+      expect(users).toEqual([user]);
+    });
+
+    // test deleting user
+    it('should delete a user', async () => {
+      await model.delete(user.id as unknown as number);
+      const users = await model.index();
+      expect(users).toEqual([]);
+    });
+  });
+  /* ==============================  END CRUD FUNCTIONALITY  ============================== */
+});
+/* ============================== END UNIT TEST ============================== */
