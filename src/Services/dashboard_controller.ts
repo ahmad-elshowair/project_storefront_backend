f3f71c69808f56734d5305a719274ca3fff9c9f5
Ahmad Elshowair <it.is.work@outlook.com>
2022-01-16 08:08:09 +0700

    retyle code of services files

--- src/Services/dashboard_controller.ts
+++ src/Services/dashboard_controller.ts
@@ -1,44 +1,44 @@
-import express, { Request, Response, Application } from "express";
-import { DashboardQueries } from "./dashboard_model";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-
+import express from 'express';
+import { DashboardQueries } from './dashboard_model';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const dashboard = new DashboardQueries();
 const { TOKEN_SECRET } = process.env;
 
-
-
 const verifyAuthToken = (
-	req: Request,
-	res: Response,
-	next: () => void
+  req: express.Request,
+  res: express.Response,
+  next: () => void
 ): void => {
-	try {
-		const authorizationHeader = req.headers['authorization'];
-		const token = authorizationHeader?.split(' ')[1] as unknown as string;
-		jwt.verify(token, TOKEN_SECRET as unknown as string);
-	} catch (error) {
-		res.status(401).json({
-			message: 'access denied you must sign up or login',
-			error: error,
-		});
-	}
-	next();
-}; 
+  try {
+    const authorizationHeader = req.headers['authorization'];
+    const token = authorizationHeader?.split(' ')[1] as unknown as string;
+    jwt.verify(token, TOKEN_SECRET as unknown as string);
+  } catch (error) {
+    res.status(401).json({
+      message: 'access denied you must sign up or login',
+      error: error,
+    });
+  }
+  next();
+};
 
-const purchased_products =async (_req: Request, res: Response) => {
+const purchased_products = async (
+  _req: express.Request,
+  res: express.Response
+) => {
   try {
     const products = await dashboard.purchasedProduct();
     res.status(200).json(products);
   } catch (error) {
     throw new Error(`${error}`);
   }
-}
+};
 
-const usersOrdered = async (_req: Request, res: Response) => {
+const usersOrdered = async (_req: express.Request, res: express.Response) => {
   try {
     const users = await dashboard.orderedUsers();
     res.status(200).json(users);
@@ -47,16 +47,19 @@ const usersOrdered = async (_req: Request, res: Response) => {
   }
 };
 
-const expensiveProducts =async (_req:Request, res:Response) => {
+const expensiveProducts = async (
+  _req: express.Request,
+  res: express.Response
+) => {
   try {
     const products = await dashboard.expensiveProducts();
     res.status(200).json(products);
   } catch (error) {
-		throw new Error(`${error}`);
+    throw new Error(`${error}`);
   }
-}
+};
 
-const dashboard_routes = (app: Application) => {
+const dashboard_routes = (app: express.Application) => {
   app.get('/purchased-products', verifyAuthToken, purchased_products);
   app.get('/users-ordered', verifyAuthToken, usersOrdered);
   app.get('/expensive-products', verifyAuthToken, expensiveProducts);
--- src/Services/dashboard_model.ts
+++ src/Services/dashboard_model.ts
@@ -1,32 +1,33 @@
-import client from "../database";
+import client from '../database';
 
-export class DashboardQueries{
-
+export class DashboardQueries {
   /* get all products that had been purchased */
-  async purchasedProduct(): Promise<{name: string, price: number, order_id: string}[]> {
+  async purchasedProduct(): Promise<
+    { name: string; price: number; order_id: string }[]
+  > {
     try {
       const connect = await client.connect();
-      const sql = 'SELECT name, price, order_id FROM products INNER JOIN order_products ON products.id = order_products.product_id';
+      const sql =
+        'SELECT name, price, order_id FROM products INNER JOIN order_products ON products.id = order_products.product_id';
       const results = await connect.query(sql);
       if (results.rows.length) {
         const products = results.rows;
         connect.release();
         return products;
       } else {
-        throw new Error("no products had been purchased yet !");
+        throw new Error('no products had been purchased yet !');
       }
     } catch (error) {
       throw new Error(`failed to bring up ordered products: ${error}`);
     }
   }
 
-
   /* get all users who over made an order */
-  async orderedUsers(): Promise<{ full_name: string, order_id: string }[]>{
+  async orderedUsers(): Promise<{ full_name: string; order_id: string }[]> {
     try {
       const connect = await client.connect();
       const sql =
-			'SELECT CONCAT(first_name, " ", last_name) as "full_name", order_id FROM users INNER JOIN orders ON users.id = orders.user_id';
+        'SELECT CONCAT(first_name, " ", last_name) as "full_name", order_id FROM users INNER JOIN orders ON users.id = orders.user_id';
       const results = await connect.query(sql);
       if (results.rows.length) {
         const users = results.rows;
@@ -41,21 +42,21 @@ export class DashboardQueries{
   }
 
   /*bring up 5 most expensive products */
-  async expensiveProducts(): Promise<{name: string, price: number}[]> {
+  async expensiveProducts(): Promise<{ name: string; price: number }[]> {
     try {
       const connect = await client.connect();
-      const sql = 'SELECT name, price FROM products WHERE price >= 1000 ORDER BY price DESC LIMIT 5';
+      const sql =
+        'SELECT name, price FROM products WHERE price >= 1000 ORDER BY price DESC LIMIT 5';
       const results = await connect.query(sql);
       if (results.rows.length) {
         const products = results.rows;
         connect.release();
         return products;
       } else {
-        throw new Error("no expensive products to bring !");
+        throw new Error('no expensive products to bring !');
       }
     } catch (error) {
       throw new Error(`Beep failed to bring up any product: ${error}`);
     }
   }
-  
 }
