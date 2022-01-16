9f618e24d23e1c9c7b0323e337c8230be061d1dc
Ahmad Elshowair <it.is.work@outlook.com>
2022-01-16 15:54:34 +0700

    modify product testing file

--- src/tests/models/Prdoucts.Spec.ts
+++ src/tests/models/Prdoucts.Spec.ts
@@ -1,69 +1,67 @@
-// import { Product, ProductStore } from "../../Models/Product";
+import { Product, ProductStore } from '../../Models/Product';
 
+const modelProduct = new ProductStore();
+const productData: Product = {
+  id: 1,
+  name: 'Nokia 8.3',
+  price: 1000,
+};
+let product: Product;
 
-// const modelProduct = new ProductStore();
+/* ==============================  BEGIN ARE CRUD FUNCTIONS DECLARED?  ============================== */
+describe('are curd functions defined', () => {
+  // define index method
+  it('should defined index method', async () => {
+    expect(modelProduct.index).toBeDefined();
+  });
 
-// /* ============================== START UNIT TEST ============================== */
-// describe('Test Product Model', () => {
-// 	/* ==============================  BEGIN ARE CRUD FUNCTIONS DECLARED?  ============================== */
-// 	describe('define curd functions', () => {
-// 		// define index method
-// 		it('should defined index method', async () => {
-// 			expect(modelProduct.index).toBeDefined();
-// 		});
+  // define show method
+  it('should defined show method', async () => {
+    expect(modelProduct.show).toBeDefined();
+  });
 
-// 		// define show method
-// 		it('should defined show method', async () => {
-// 			expect(modelProduct.show).toBeDefined();
-// 		});
+  // define create method
+  it('should defined create method', async () => {
+    expect(modelProduct.create).toBeDefined();
+  });
 
-// 		// define create method
-// 		it('should defined create method', async () => {
-// 			expect(modelProduct.create).toBeDefined();
-// 		});
+  // define edit method
+  it('should defined edit method', async () => {
+    expect(modelProduct.update).toBeDefined();
+  });
 
-// 		// define edit method
-// 		it('should defined edit method', async () => {
-// 			expect(modelProduct.update).toBeDefined();
-// 		});
+  // define delete method
+  it('should defined delete method', async () => {
+    expect(modelProduct.delete).toBeDefined();
+  });
+});
+/* ==============================  END ARE CRUD FUNCTIONS DECLARED?  ============================== */
 
-// 		// define delete method
-// 		it('should defined delete method', async () => {
-// 			expect(modelProduct.delete).toBeDefined();
-// 		});
-// 	});
-// 	/* ==============================  END ARE CRUD FUNCTIONS DECLARED?  ============================== */
+/* ==============================  BEGIN CURD FUNCTIONALITY  ============================== */
+describe('Test CURD functionality', () => {
+  // create a new product
+  it('should create new product', async () => {
+    product = await modelProduct.create(productData);
+    expect({
+      name: product.name,
+      price: product.price,
+    }).toEqual({
+      name: productData.name,
+      price: productData.price,
+    });
+  });
 
-//   /* ==============================  BEGIN CURD FUNCTIONALITY  ============================== */
-//   describe("Test CURD functionality", () => {
+  /* test index method functionality */
+  it('index method should return products', async () => {
+    const products = await modelProduct.index();
+    expect(products).toEqual([product]);
+  });
 
-//     const productData: Product = {
-//       name: 'Nokia 8.3',
-//       price: 1000,
-// 	  };
-//     let product: Product;
-    
-//     // create a new product
-//     it('should create new product', async () => {
-//       product = await modelProduct.create(productData);
-//       expect({
-//         name: product.name,
-//         price: product.price
-//       }).toEqual({
-//         name: productData.name,
-//         price: productData.price
-//       });
-//     });
-
-//     /* test index method functionality */
-//     it('index method should return products', async () => {
-//       const products = await modelProduct.index();
-//       expect(products).toEqual([product]);
-//     });
-
-//   });
-// 	/* ==============================  END CURD FUNCTIONALITY  ============================== */
-// });
-
-
-// /* ============================== END UNIT TEST ============================== */
+  // test deleting product
+  it('should delete the product', async () => {
+    await modelProduct.delete(product.id as unknown as number);
+    const products = await modelProduct.index();
+    expect(products).toEqual([]);
+  });
+});
+/* ==============================  END CURD FUNCTIONALITY  ============================== */
