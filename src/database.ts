2939596224fe2f8407fcc8a8c7ab1101df302459
Ahmad Elshowair <it.is.work@outlook.com>
2022-01-17 12:59:33 +0700

    modify database file by adding the database port

--- src/database.ts
+++ src/database.ts
@@ -6,7 +6,9 @@ dotenv.config();
 
 const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 
+const dbPort = process.env.DB_PORT as unknown as number;
 const client = new Pool({
+  port: dbPort,
   host: DB_HOST,
   database: DB_NAME,
   user: DB_USER,
