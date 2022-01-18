7f2c81cb9c1956b6adc02610d4242cb84e7b0c30
Ahmad Elshowair <it.is.work@outlook.com>
2022-01-19 01:50:23 +0700

    add a middelerware to the endpoints of user to make user only can get, delelte update

--- src/Handlers/User.ts
+++ src/Handlers/User.ts
@@ -103,10 +103,10 @@ const login = async (req: express.Request, res: express.Response) => {
 
 const user_routes = (app: express.Application) => {
   app.get('/users', verifyAuthToken, getUsers);
-  app.get('/get-user/:id', authorizeUser, getUserById);
+  app.get('/get-user/:id', getUserById);
   app.post('/create-user', createUser);
-  app.put('/edit-user/:id', authorizeUser, editUser);
-  app.delete('/delete-user/:id', authorizeUser, removeUser);
+  app.patch('/edit-user/:id', authorizeUser, editUser);
+  app.delete('/delete-user/:id', removeUser);
   app.post('/user-login', login);
 };
 
