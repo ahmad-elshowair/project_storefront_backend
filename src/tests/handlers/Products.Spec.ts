70cd2b661b15c6f7a5cb28d8878f96857e900a21
Ahmad Elshowair <it.is.work@outlook.com>
2022-01-18 17:48:42 +0700

    add specs testing of the products endpoints

---
+++ src/tests/handlers/Products.Spec.ts
@@ -0,0 +1,85 @@
+import supertest from 'supertest';
+import { Product } from '../../Models/Product';
+import app from '../../server';
+import { User } from '../../Models/User';
+import client from '../../database';
+
+const request = supertest(app);
+
+describe('TEST PRODUCTS ENDPOINTS', () => {
+  const product_data: Product = {
+    name: 'iphone',
+    price: 1000,
+  };
+  let product_id: number;
+  let token: string;
+
+  beforeAll(async () => {
+    const user_data: User = {
+      first_name: 'Ahmad',
+      last_name: 'Elshowair',
+      email: 'elshowair@mail.com',
+      password_digest: '1933',
+    };
+    await request
+      .post('/create-user')
+      .send(user_data)
+      .expect(200)
+      .then((res) => {
+        token = res.body;
+      });
+  });
+
+  // test the endpoint of /create-product method
+  it('Should return OK with the endpoint /create-product', async () => {
+    await request
+      .post('/create-product')
+      .set('Authorization', `Bearer ${token}`)
+      .send(product_data)
+      .expect(200)
+      .then((res) => {
+        expect(res.body.price).toEqual(1000);
+        product_id = res.body.id;
+        console.log(product_id);
+      });
+  });
+
+  // test the endpoint of /products method
+  it('Should return OK with the endpoint of /products', async () => {
+    await request.get('/products').expect(200);
+  });
+
+  // test the endpoint of /get-product method
+  it('Should return OK with the endpoint of /products', async () => {
+    await request
+      .get(`/get-product/${product_id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
+  });
+
+  // test the endpoint of  /edit-product
+  it('Should return OK with the endpoint of /edit-product', async () => {
+    const edit_data: Product = {
+      name: 'nokia',
+      price: 500,
+    };
+    await request
+      .put(`/edit-product/${product_id}`)
+      .send(edit_data)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
+  });
+
+  // test the endpoint of /delete-product
+  it('Should return OK with the endpoint of /delete-product', async () => {
+    await request
+      .del(`/delete-product/${product_id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
+  });
+
+  // after all test delete the user that had created before all tests
+  afterAll(async () => {
+    await client.query('DELETE FROM users');
+  });
+});
