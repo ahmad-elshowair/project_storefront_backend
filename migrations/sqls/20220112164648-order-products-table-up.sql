027bc254cfd34250ad358e68bd99ba881fab8d34
Ahmad Elshowair <it.is.work@outlook.com>
2022-01-18 22:44:34 +0700

    change type of the fk

--- migrations/sqls/20220112164648-order-products-table-up.sql
+++ migrations/sqls/20220112164648-order-products-table-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE order_products ( 
 id SERIAL PRIMARY KEY,
   quantity INTEGER,
-order_id INTEGER REFERENCES orders(id),
-product_id INTEGER REFERENCES products(id)
+order_id INT REFERENCES orders(id),
+product_id INT REFERENCES products(id)
 );
